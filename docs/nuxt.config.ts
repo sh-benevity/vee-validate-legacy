f74e28172a1a4333ced5b8f32f9098e5de37f6f8
Vincent Girard <vinccool96@gmail.com>
2023-03-28 14:04:33 -0400

    Fixes

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: "/vee-validate-legacy",
+  },
+
   modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss"],
+
+  i18n: {
+    locales: [
+      {
+        code: "en_US",
+        file: "en_US.ts",
+      },
+      {
+        code: "fr_CA",
+        file: "fr_CA.ts",
+      },
+    ],
+    defaultLocale: "en_US",
+    langDir: "./lang",
+    strategy: "no_prefix",
+  },
 })
