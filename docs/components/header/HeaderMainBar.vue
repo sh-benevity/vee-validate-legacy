f090862e7ce0bfacc9ee69a38518da6112af2342
Vincent Girard <vinccool96@gmail.com>
2023-10-25 11:44:22 -0400

    Update unbuild and typescript

--- docs/components/header/HeaderMainBar.vue
+++ docs/components/header/HeaderMainBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="border-b-2 px-4 flex items-stretch md:px-6 relative">
-    <NuxtLink to="/">VeeValidateLegacy</NuxtLink>
+    <NuxtLink to="/" class="flex-grow md:ml-2 xl:ml-0 py-4 px-3">VeeValidateLegacy</NuxtLink>
+    <div class="flex-grow-0 top-full left-0 py-4 px-3">Sup</div>
   </div>
 </template>
 
